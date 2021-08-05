491afe1924f9275c6fb97fef1a72df5a4e877b31
Valentina Corak <valentinacorak@gmail.com>
2021-08-05 13:54:13 +0200

    JS for Carousel is working, just not satisfied with scroller, have to fix it...

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function randomLetter(){
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', evt => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', evt => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const scrollSpeed = 3;
+    const walk = (x - startX) * scrollSpeed;
+    slider.scrollLeft = scrollLeft - walk;
+})
+
